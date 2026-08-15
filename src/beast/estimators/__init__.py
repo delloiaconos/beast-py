0a067de7bafd73f787f7b1dc8e252e4c0c564d48
Salvatore Dello Iacono <delloiaconos@gmail.com>
2026-08-15 12:30:17 +0200

    Updated estimators submodule.

--- src/beast/estimators/__init__.py
+++ src/beast/estimators/__init__.py
@@ -1,7 +1,15 @@
 """Estimator algorithms."""
 
 from beast.estimators.base import Estimator
+from beast.estimators.ekf_dual import Estimator_EKFdual
+from beast.estimators.enhanced_mix_algorithm import Estimator_EnhancedMixAlgorithm
+from beast.estimators.mix_algorithm import Estimator_MixAlgorithm
+from beast.estimators.open_loop import Estimator_OpenLoop
 
 __all__ = [
     "Estimator",
+    "Estimator_EKFdual",
+    "Estimator_EnhancedMixAlgorithm",
+    "Estimator_MixAlgorithm",
+    "Estimator_OpenLoop",
 ]
