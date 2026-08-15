da2448cce7fd2c681376fa501a8d23b90dc290a3
Salvatore Dello Iacono <delloiaconos@gmail.com>
2026-08-15 16:12:20 +0200

    Updated generators packages and added test for pulse.

---
+++ src/beast/generators/__init__.py
@@ -0,0 +1,6 @@
+"""Input-profile generators for BEAST simulations."""
+
+from beast.generators.base import Generator
+from beast.generators.pulse import Generator_Pulse
+
+__all__ = ["Generator", "Generator_Pulse"]
