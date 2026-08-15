97b0bcc3325cbc0901cfaa3c6e41283f9d42ddb7
Salvatore Dello Iacono <delloiaconos@gmail.com>
2026-08-15 13:07:31 +0200

    Updated beast module.

--- src/beast/__init__.py
+++ src/beast/__init__.py
@@ -8,5 +8,64 @@ The framework uses Python-native data structures, NumPy arrays, and modular
 components to support research, simulation, and algorithm development.
 """
 
+from beast.core.arrays import (
+    FloatArray, 
+    IntArray
+)
+
+from beast.core.data import (
+    ConfigData,
+    InputData,
+    PlainData,
+    ResultData,
+    normalize_covariance,
+)
+
+
 from beast.cell_models import (
-    CellModel )
+    CellModel,
+    CellModel_H0F0A,
+    CellModel_R0A1B1,
+    CellModel_R0R1C1,
+    CellModel_R0R1C1R2C2,
+    CellModel_R0R1T1,
+    initCellModel,
+    selectCellModel,
+    createCellModel,
+)
+
+from beast.estimators import (
+    Estimator,
+    Estimator_EKFdual,
+    Estimator_EnhancedMixAlgorithm,
+    Estimator_MixAlgorithm,
+    Estimator_OpenLoop,
+    selectEstimator,
+    createEstimator,
+)
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "__version__",
+    
+    "FloatArray", "IntArray",
+    "ConfigData", "InputData", "PlainData", "ResultData",
+    "normalize_covariance",
+
+    "CellModel",
+    "CellModel_H0F0A",
+    "CellModel_R0A1B1",
+    "CellModel_R0R1C1",
+    "CellModel_R0R1C1R2C2",
+    "CellModel_R0R1T1",
+    "initCellModel", "selectCellModel", "createCellModel",
+
+    "Estimator",
+    "Estimator_EKFdual",
+    "Estimator_EnhancedMixAlgorithm",
+    "Estimator_MixAlgorithm",
+    "Estimator_OpenLoop",
+    "selectEstimator", "createEstimator",
+]
+
