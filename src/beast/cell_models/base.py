cc287c57adf64df726fdd6b3c3193e054447c94e
Salvatore Dello Iacono <delloiaconos@gmail.com>
2026-08-15 13:01:13 +0200

    Added CellModel_Dummy

--- src/beast/cell_models/base.py
+++ src/beast/cell_models/base.py
@@ -191,3 +191,34 @@ class CellModel(ABC):
             raise ValueError(f"u_all must have {cls.Nu} rows, got {u_all.shape[0]}")
 
     CellModelDimCheck = CheckCellModelDim
+
+
+
+class CellModel_Dummy(CellModel):
+
+    def __init__(self):
+        self.Nx = 2
+        self.Np = 2
+        self.Nu = 1
+        self.Ny = 1
+
+    def f0(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def g0(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def f1x(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def f1p(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def g1x(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def g1p(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def coerce_parameters(cls, parameters: Any) -> FloatArray:
+        raise NotImplementedError
